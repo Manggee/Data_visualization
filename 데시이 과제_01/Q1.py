57f03e4e849f3eacd3ae81021657cc7af88df2d5
mangG907 <mang.gee97@gmail.com>
2025-03-28 11:28:58 +0900

    데이터시각화 이해와 실습 공부

---
+++ 데시이 과제_01/Q1.py
@@ -0,0 +1,42 @@
+import csv
+
+f = open("heat_wave.csv", 'r', encoding="cp949")
+data = csv.reader(f)
+
+# 아래부터 솜솜이가 작성해야 할 부분
+
+# 헤더 5줄 건너뛰기
+for i in range(5):
+    next(data)
+
+# 변수 초기화 해주기
+year = ''
+location = ''
+start_date = ''
+end_date = ''
+max_duration = 0  # 가장 긴 지속일수 저장
+
+for row in data:
+    if row[4] != '':  # 지속일수가 존재하는지 확인
+        current_duration = int(row[4])
+        if current_duration > max_duration:
+            max_duration = current_duration
+            year = row[0]
+            location = row[1]
+            start_date = row[2]
+            end_date = row[3]
+            duration = int(row[4])
+
+# 시작일/종료일에서 월, 일을 분리
+start_month = start_date[5:7]
+start_day = start_date[8:]
+end_month = end_date[5:7]
+end_day = end_date[8:]
+
+print(f"폭염 지속일수가 가장 길었던 연도:{year}년")
+print(f"해당 지점:{location}")
+print(f"폭염 시작일:{start_month}월 {start_day}일")
+print(f"폭염 종료일:{end_month}월 {end_day}일")
+print(f"지속일수:{max_duration}일")
+
+f.close()
---
+++ 데시이 과제_01/Q2.py
@@ -0,0 +1,51 @@
+import csv
+import matplotlib.pyplot as plt
+
+f = open("sr_profit.csv", 'r', encoding="cp949")
+data = csv.reader(f)
+
+header = next(data)
+
+# 열 인덱스 딕셔너리 생성
+target_cols = {}
+for i, col_name in enumerate(header):
+    if '경부선' in col_name:
+        year_part = col_name.split('년')[0] # '년'을 기준으로 나눠 연도만 추출해옴
+        if year_part.isdigit() and int(year_part) % 2 != 0: # 연도가 숫자로만 이루어져있고 홀수 연도여야함 (공집합
+            target_cols[year_part] = i # 추출해야 하는 열 인덱스 {'2017': 3, '2019': 9, ...}
+
+# target_cols 딕셔너리에서 key값(연도)만 가져온 후 정렬
+sorted_years = sorted(target_cols.keys())
+
+# 각 연도별로 수익 데이터를 저장할 빈 딕셔너리 생성
+profits = {y:[] for y in sorted_years}
+
+# 각 연도별 월별 수익값 저장
+for row in data:
+    for y in sorted_years:
+        value = row[target_cols[y]]
+        if value != '':
+            profits[y].append(int(value))
+
+f.close()
+
+colors = ['r', 'g', 'b', 'k']
+markers = ['^', '>', 'v', '<']
+months = list(range(1, 13))
+
+# 그래프 그리기
+for i, y in enumerate(sorted_years):
+    plt.plot(
+        months,
+        profits[y],
+        color=colors[i],
+        marker=markers[i],
+        linewidth=0.5,
+        label=y
+    )
+
+plt.xlabel("Month")
+plt.ylabel("Profit")
+plt.legend()
+plt.grid(True)
+plt.show()
